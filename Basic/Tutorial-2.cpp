606979536c6e92ab63d3f0b44e1cf916b1150b64
Harsha Vardhini <harshavardhinipillalamarri@gmail.com>
2026-01-18 17:26:37 +0530

    Variables and Data Types

---
+++ Basic/Tutorial-2.cpp
@@ -0,0 +1,66 @@
+                            //Data Types in C++
+//Basic Data Types
+/*
+int,float,double,char,bool,void
+*/
+
+//Derived Data Types
+/*
+array,pointer,reference,function
+*/
+
+//User Defined Data Types
+/*
+class,structure,union,enumeration,typedef,using
+*/
+
+
+/*
+Integer Data Type: int
+#include <iostream>
+using namespace std;
+
+int main() {
+
+  	// Creating a variable to store integer
+    int var = 10;
+  
+  	cout << var;
+    return 0;
+}
+Output:
+10
+*/
+
+/* 
+Character Data Type: char
+//Example: Finding ASCII value of a character
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    char ch = 'C';
+    cout<<"The ASCII value of "<<ch<<" is "<<int(ch)<<endl;
+    return 0;
+}
+Output:
+The ASCII value of C is 67
+*/
+
+/*
+Bool data type
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    bool isPrime = true;
+    bool isEven = false;
+    cout << "isPrime: " << isPrime << endl; // Outputs 1 for true
+    cout << "isEven: " << isEven << endl;// Outputs 0 for false
+    return 0;
+}
+*/
+
